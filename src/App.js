6880766a9fa8368c6848f5e501db155f89a75a8c
zhuwenqian <1506675573@qq.com>
2018-12-24 11:09:17 +0800

    add PriceList components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PriceList from './components/PriceList';
+const items = [
+  {
+    "id": 1,
+    "title": "去云南旅游",
+    "price": 200,
+    "date": "2018-09-10",
+    "category": {
+    "id": 1,
+    "name": "旅行",
+      "type": "outcome"
+    }
+  },
+  {
+    "id": 2,
+    "title": "去云南旅游",
+    "price": 200,
+    "date": "2018-09-10",
+    "category": {
+    "id": 1,
+    "name": "旅行",
+      "type": "outcome"
+    }
+  }
+
+]
 
 class App extends Component {
   render() {
@@ -8,18 +35,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <PriceList
+          items={items}
+          onModifyItem={(item) => {alert(item.id)}}
+          onDeleteItem={(item) => {alert(item.id)}}
+        />
       </div>
     );
   }
